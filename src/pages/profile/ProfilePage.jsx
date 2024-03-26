30f2e9972c9a0b7034698d36b487e8c05f0d2460
melolidas <the_halidcastle@mail.ru>
2024-03-26 11:40:22 +0600

    profile

--- src/pages/profile/ProfilePage.jsx
+++ src/pages/profile/ProfilePage.jsx
@@ -1,27 +1,43 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import "./profile.css";
 
 const ProfilePage = () => {
   const { currentUser } = useSelector((state) => state.users);
-  console.log(currentUser);
+
   return (
-    <div>
+    <div className="profile-container">
       {currentUser && (
-        <div>
-          <img src={currentUser.backgroundImage} alt="" />
-          <img src={currentUser.profileImage} alt="" />
-          <h2>{currentUser.username}</h2>
-          <p>My BIO: {currentUser.description}</p>
-          <h2>My posts:</h2>
-          <div>
-            {currentUser.posts.map((item) => (
-              <div key={item.id}>
-                <img src={item.image} alt="" />
-                <h2>{item.title}</h2>
-                <p>{item.author}</p>
-                <p>{item.description}</p>
-              </div>
-            ))}
+        <div className="profile">
+          <div className="profile-header">
+            <img
+              className="background-image"
+              src={currentUser.backgroundImage}
+              alt=""
+            />
+            <img
+              className="profile-image"
+              src={currentUser.profileImage}
+              alt=""
+            />
+            <h2 className="username">{currentUser.username}</h2>
+            <p className="bio">My BIO: {currentUser.description}</p>
+          </div>
+          <div className="profile-posts">
+            <h2 className="posts-title">My posts:</h2>
+            <div className="posts-list">
+              {currentUser.posts.map((item) => (
+                <div className="post" key={item.id}>
+                  <p className="post-author">Author: {item.author}</p>
+                  <img className="post-image" src={item.image} alt="" />
+                  <div className="post-details">
+                    <h3 className="post-title">{item.title}</h3>
+
+                    <p className="post-description">{item.description}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       )}
