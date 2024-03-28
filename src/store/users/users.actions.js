6577281a11b6bf6d94615e78cebf7a2662480657
tikhomirowww <tihomirovi92@gmail.com>
2024-03-28 14:15:39 +0600

    .

--- src/store/users/users.actions.js
+++ src/store/users/users.actions.js
@@ -55,6 +55,28 @@ export const handleLike = createAsyncThunk(
   }
 );
 
+export const createComment = createAsyncThunk(
+  "users/createComment",
+  async ({ user, postId, author, comment }, { dispatch }) => {
+    const updatedPosts = {
+      ...user,
+      posts: user.posts.map((item) => {
+        if (item.id === postId) {
+          return {
+            ...item,
+            comments: [...item.comments, { author, comment }],
+          };
+        } else {
+          return item;
+        }
+      }),
+    };
+
+    await axios.patch(`${USERS_API}/${user.id}`, updatedPosts);
+    dispatch(getOneUser(user.id));
+  }
+);
+
 export const editProfile = createAsyncThunk(
   "users/editProfile",
   async ({ user, id }, { dispatch }) => {
--- src/widgets/card/Card.jsx
+++ src/widgets/card/Card.jsx
@@ -1,26 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { FcLikePlaceholder } from "react-icons/fc";
 import { FcLike } from "react-icons/fc";
 import Input from "../inputs/Input";
 import Button from "../buttons/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { createComment } from "../../store/users/users.actions";
 
 const Card = ({ item, toggleLike, likedPosts, showComment, toggleComment }) => {
+  const [comment, setComment] = useState("");
+  const { currentUser, oneUser } = useSelector((state) => state.users);
+  const dispatch = useDispatch();
   return (
     <div className="post" key={item.id}>
       <img className="post-image" src={item.image} alt="" />
-      <div onClick={() => toggleLike(item.id)}>
-        {likedPosts.has(item.id) ? (
-          <FcLike size={25} />
-        ) : (
-          <FcLikePlaceholder size={25} />
-        )}
-      </div>
-      <p>{item.likes}</p>
+      {currentUser && (
+        <div onClick={() => toggleLike(item.id)}>
+          {likedPosts.has(item.id) ? (
+            <FcLike size={25} />
+          ) : (
+            <FcLikePlaceholder size={25} />
+          )}
+        </div>
+      )}
+      <p>Likes: {item.likes}</p>
       <div className="post-details">
         <h3 className="post-title">{item.title}</h3>
         <p className="post-description">{item.description}</p>
-        <div>{showComment.has(item.id) && <Input />}</div>
-        <Button onClick={() => toggleComment(item.id)}>Comment</Button>
+        <div>
+          Comments:
+          {item.comments.map((item, index) => (
+            <div key={index}>
+              <b>{item.author}:</b> {item.comment}
+            </div>
+          ))}
+        </div>
+        <div>
+          {showComment.has(item.id) && (
+            <Input
+              name="Comment"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            />
+          )}
+        </div>
+        {currentUser && (
+          <Button
+            onClick={() => {
+              if (comment) {
+                dispatch(
+                  createComment({
+                    user: oneUser,
+                    postId: item.id,
+                    author: currentUser.username,
+                    comment,
+                  })
+                );
+                setComment("");
+              }
+              toggleComment(item.id);
+            }}
+          >
+            {showComment.has(item.id) ? "Send" : "Comment"}
+          </Button>
+        )}
       </div>
     </div>
   );
